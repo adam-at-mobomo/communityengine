0a37f1c114b3c38b3c1598aa6ab0c1ff50c7d345
Adam Frazin <adam@mobomo.com>
2012-04-17 19:17:55 -0400

    Fix migration name for Create Community Engine Activities Table

---
+++ db/migrate/039_create_community_engine_activities_table.rb
@@ -0,0 +1,17 @@
+class CreateCommunityEngineActivitiesTable < ActiveRecord::Migration
+  def self.up
+    create_table :community_engine_activities do |t|
+      t.column :user_id, :integer, :limit => 10
+      t.column :action, :string, :limit => 50
+      t.column :item_id, :integer, :limit => 10
+      t.column :item_type, :string
+      t.column :created_at, :datetime
+    end
+
+
+  end
+
+  def self.down
+    drop_table :community_engine_activities
+  end
+end
