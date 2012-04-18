df0368e6e26329a31926b53c2e6fc0e0466fba5a
Adam Frazin <adam@mobomo.com>
2012-04-18 01:28:41 -0400

    Fix usage of FriendlyId version 4

--- app/models/community_engine/category.rb
+++ app/models/community_engine/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   has_many :posts, :order => "published_at desc"
   validates_presence_of :name
   
-  has_friendly_id :name, :use_slug => true
+  friendly_id :name, :use_slug => true
       
   def display_new_post_text
     new_post_text
