558376606df4bb0ed9d9f267e9698cf993fa5daa
Adam Frazin <adam@mobomo.com>
2012-04-18 00:52:51 -0400

    Use newer version of friendly_id

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -3,7 +3,7 @@ module CommunityEngine
      MAJOR = 2
      MINOR = 0
      PATCH = 0
-     BUILD = 'beta4dev6'
+     BUILD = 'beta4dev7'
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
