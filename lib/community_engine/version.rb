ceb395a9602ae716e3497b5c94911ad71edce0a1
Adam Frazin <adam@mobomo.com>
2013-04-08 17:43:46 -0400

    Bump version

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -3,7 +3,7 @@ module CommunityEngine
      MAJOR = 2
      MINOR = 0
      PATCH = 0
-     BUILD = 'beta4dev105'
+     BUILD = 'beta4dev106'
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
