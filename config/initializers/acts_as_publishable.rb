0e4a8fccde5cb48d6919816d35af56403344d765
Adam Frazin <adam@mobomo.com>
2013-04-11 14:02:43 -0400

    Move acts_as_publishable from plugin to lib to support Rails 4.0+

--- config/initializers/acts_as_publishable.rb
+++ config/initializers/acts_as_publishable.rb
@@ -1,3 +1,3 @@
-require CommunityEngine::Engine.config.root.join('lib', 'acts_as_publishable')
+require CommunityEngine::Engine.config.root.join('lib', 'acts_as_publishable', 'acts_as_publishable')
 ActiveRecord::Base.send(:include, Acts::As::Publishable)
 
