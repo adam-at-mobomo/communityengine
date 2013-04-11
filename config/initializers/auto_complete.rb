e1931c6da0aa09c21f67f83e7644bc1a81ff8dc7
Adam Frazin <adam@mobomo.com>
2013-04-11 19:35:11 -0400

    Move auto_complete from plugin to lib to support Rails 4.0+

--- config/initializers/auto_complete.rb
+++ config/initializers/auto_complete.rb
@@ -1,2 +1,4 @@
+require CommunityEngine::Engine.config.root.join('lib', 'auto_complete', 'auto_complete')
+require CommunityEngine::Engine.config.root.join('lib', 'auto_complete', 'auto_complete_macros_helper')
 ActionController::Base.send :include, AutoComplete
 ActionController::Base.helper AutoCompleteMacrosHelper
